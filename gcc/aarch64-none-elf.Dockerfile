baa090abc4b434fdaefffa234105a427464f2b9a
chrissx <chrissx@chrissx.de>
2021-03-30 14:55:21 +0200

    lets just try crazy things

--- gcc/aarch64-none-elf.Dockerfile
+++ gcc/aarch64-none-elf.Dockerfile
@@ -1,7 +1,10 @@
-FROM chrissx/gcc-deps:10.2
+FROM ubuntu:latest
 
 ARG BINUTILS_VERSION=2.36.1
 ARG GCC_VERSION=10.2.0
 
-RUN sh /usr/src/build-binutils.sh $BINUTILS_VERSION aarch64-none-elf
-RUN sh /usr/src/build-gcc.sh $GCC_VERSION aarch64-none-elf
+RUN mkdir -p /usr/src/
+COPY *.tar.gz /usr/src/
+COPY *.sh /usr/src/
+
+RUN sh /usr/src/build.sh $BINUTILS_VERSION $GCC_VERSION aarch64-none-elf
---
+++ gcc/build.sh
@@ -0,0 +1,31 @@
+export DEBIAN_FRONTEND=noninteractive && \
+apt update && \
+apt upgrade -y && \
+apt install -y build-essential libgmp3-dev libmpfr-dev libisl-dev \
+               libmpc-dev texinfo bison curl flex && \
+cd /usr/src/ && \
+tar xzf binutils-$1.tar.gz && \
+rm -f binutils-$1.tar.gz && \
+chown -R root:root binutils-$1 && \
+chmod -R o-w,g+w binutils-$1 && \
+mkdir build-binutils && \
+cd build-binutils && \
+../binutils-$1/configure --target=$3 --prefix=/usr --program-prefix=$3- --with-sysroot --disable-nls --disable-werror && \
+make -j$(nproc) && \
+make install && \
+cd /usr/src/ && \
+tar xzf gcc-$2.tar.gz && \
+rm -f gcc-$2.tar.gz && \
+chown -R root:root gcc-$2 && \
+chmod -R o-w,g+w gcc-$2 && \
+mkdir build-gcc && \
+cd build-gcc && \
+../gcc-$2/configure --target=$3 --prefix=/usr --program-prefix=$3- --disable-nls --enable-languages=c,c++ --without-headers && \
+make -j$(nproc) all-gcc && \
+make -j$(nproc) all-target-libgcc && \
+make install-gcc && \
+make install-target-libgcc && \
+cd /usr/src && \
+rm -rf build.sh build-binutils binutils-$1 build-gcc gcc-$2 && \
+apt remove -y build-essential gcc make libgmp3-dev libmpfr-dev libisl-dev \
+              libmpc-dev texinfo bison curl flex
