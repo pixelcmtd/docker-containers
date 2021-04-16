99f7ca9a53b931499e911a040cf9801552684513
chrissx <chrissx@chrissx.de>
2021-04-17 00:48:52 +0200

    lets just use polipo because it "works"

--- tor-proxy/run.sh
+++ tor-proxy/run.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+# hack, but it works
+{ sleep 3 ; polipo ; } &
 tor &
 
 while true ; do
+
+sleep 300
 
 cat << 'EOF' | nc localhost 9051
 authenticate ""
 signal newnym
 quit
 EOF
-
-sleep 60
 
 done
