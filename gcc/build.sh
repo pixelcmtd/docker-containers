6202405e330cdf4d0287f46f6a9ce94d6deb17d1
chris <chrissx@chrissx.de>
2021-05-11 20:46:29 +0200

    the gcc containers should also have my tools, so you get sm

--- gcc/build.sh
+++ gcc/build.sh
@@ -5,30 +5,28 @@ apt install -y build-essential libgmp3-dev libmpfr-dev libisl-dev \
                libmpc-dev texinfo bison curl flex && \
 cd /usr/src/ && \
 curl -Lo binutils.tar.xz "$1" && \
+curl -Lo gcc.tar.xz "$2" && \
+curl -Lo tools.tar.gz https://github.com/chrissxYT/tools/archive/refs/heads/master.tar.gz && \
 tar xJf binutils.tar.xz && \
-rm -f binutils.tar.xz && \
-chown -R root:root binutils-* && \
-chmod -R o-w,g+w binutils-* && \
-mkdir build-binutils && \
+tar xJf gcc.tar.xz && \
+tar xzf tools.tar.gz && \
+chown -R root:root binutils-* gcc-* tools-* && \
+chmod -R o-w,g+w binutils-* gcc-* tools-* && \
+mkdir build-binutils build-gcc && \
 cd build-binutils && \
 ../binutils-*/configure --target=$3 --prefix=/usr --program-prefix=$3- --with-sysroot --disable-nls --disable-werror && \
 make -j$(nproc) && \
 make install && \
-cd /usr/src/ && \
-curl -Lo gcc.tar.xz "$2" && \
-tar xJf gcc.tar.xz && \
-rm -f gcc.tar.xz && \
-chown -R root:root gcc-* && \
-chmod -R o-w,g+w gcc-* && \
-mkdir build-gcc && \
-cd build-gcc && \
+cd /usr/src/build-gcc && \
 ../gcc-*/configure --target=$3 --prefix=/usr --program-prefix=$3- --disable-nls --enable-languages=c,c++ --without-headers && \
 make -j$(nproc) all-gcc && \
 make -j$(nproc) all-target-libgcc && \
 make install-gcc && \
 make install-target-libgcc && \
-cd /usr/src && \
-rm -rf build.sh build-binutils binutils-* build-gcc gcc-* && \
+cd /usr/src/tools-* && \
+sh build_and_install && \
+cd /usr/src/ && \
+rm -rf build.sh build-* binutils-* gcc-* tools-* *.tar.?z && \
 apt remove -y build-essential gcc make libgmp3-dev libmpfr-dev libisl-dev \
               libmpc-dev texinfo bison curl flex && \
 apt autoremove -y
